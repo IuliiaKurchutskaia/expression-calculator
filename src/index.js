e473161ec9d265c796cd1bf2c5f0f7c4569fa31c
kud92@icloud.com <kud92@icloud.com>
2021-08-21 18:09:57 +0300

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -4,8 +4,33 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let arr = expr.replace(/\s/g,"").split("");
+    error(arr);
+    function error(arr) {
+      let isPaired = 0;
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === "(") {
+          isPaired++;
+        }
+        if (arr[i] === ")") {
+          isPaired--;
+        }
+      }
+      if (isPaired !== 0) {
+        throw new Error("ExpressionError: Brackets must be paired.");
+      }
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === "/") {
+          if (arr[i+1] === "0") {
+            throw new Error("TypeError: Division by zero.");
+          }
+        }
+      }
+    }
+    result = new Function("return " + expr.replace(/\s/g,""));
+    return result();
 }
+
 
 module.exports = {
     expressionCalculator
